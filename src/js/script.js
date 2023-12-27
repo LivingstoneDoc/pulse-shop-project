ae13e2834018267a1dec25c10fed0693c173b209
LivingstoneDoc <livingstone.doc@yandex.ru>
2023-12-27 11:44:15 +0400

    feat: add letters sending from website to server

--- src/js/script.js
+++ src/js/script.js
@@ -74,6 +74,21 @@ $(document).ready(function(){
     formValidation("#order form");
 
     $('input[name=phone]').mask('+7 (999) 999-99-99');
+
+    $("form").submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn();
+        $("form").trigger("reset");
+      });
+      return false;
+    });
   
   });
   
