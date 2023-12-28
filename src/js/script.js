ab455912621d541f97891a78fba29a20d903861d
LivingstoneDoc <livingstone.doc@yandex.ru>
2023-12-28 12:19:38 +0400

    feat: add pageup element, smooth scroll

--- src/js/script.js
+++ src/js/script.js
@@ -89,6 +89,20 @@ $(document).ready(function(){
       });
       return false;
     });
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1138) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+  
+    $('a[href^="#"]').click(function() {
+      const _href = $(this).attr('href');
+      $('html, body').animate({scrollTop: $(_href).offset().top+'px'});
+      return false;
+    });
   
   });
   
